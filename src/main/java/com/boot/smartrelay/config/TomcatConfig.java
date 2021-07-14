e4ac7f0b2fa67b670f5b1e2e3e1562c1e2333a27
23397 <ryu.hs@gsshop.com>
2021-07-15 07:57:18 +0900

    [hyunsoo] - https 제거

--- src/main/java/com/boot/smartrelay/config/TomcatConfig.java
+++ src/main/java/com/boot/smartrelay/config/TomcatConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class TomcatConfig {
 
